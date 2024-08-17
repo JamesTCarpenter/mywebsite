7ed648806c00d74c70571f92fe0655edea64443d
James Carpenter <jamestimothycarpenter@gmail.com>
2024-08-17 14:27:15 +0100

    Update index.js

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,58 +1,42 @@
-import React from 'react'
-import { useState } from 'react'
-import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
-import ProjectCard from '../Cards/ProjectCards'
-import { projects } from '../../data/constants'
+import React, { useState } from 'react';
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle';
+import ProjectCard from '../Cards/ProjectCards';
+import { projects } from '../../data/constants';
 
-
-const Projects = ({openModal,setOpenModal}) => {
+const Projects = ({ openModal, setOpenModal }) => {
   const [toggle, setToggle] = useState('all');
+
   return (
     <Container id="projects">
       <Wrapper>
         <Title>Projects</Title>
         <Desc>
-          I have worked on a wide range of websites. Recently I have been working on websites in the charity and hospitality sectior.
+          I have worked on a wide range of websites. Recently, I have been working on websites in the charity and hospitality sectors.
         </Desc>
-        <ToggleButtonGroup >
-          {toggle === 'featured' ?
-            <ToggleButton active value="featured" onClick={() => setToggle('featured')}>Featured</ToggleButton>
-            :
-            <ToggleButton value="featured" onClick={() => setToggle('featured')}>Featured</ToggleButton>
-          }
-          <Divider />
-          {toggle === 'hospitality' ?
-            <ToggleButton active value="hospitality" onClick={() => setToggle('hospitality')}>Hospitality</ToggleButton>
-            :
-            <ToggleButton value="hospitality" onClick={() => setToggle('hospitality')}>Hospitality</ToggleButton>
-          }
-          <Divider />
-          {toggle === 'charity' ?
-            <ToggleButton active value="charity" onClick={() => setToggle('charity')}>Charity</ToggleButton>
-            :
-            <ToggleButton value="charity" onClick={() => setToggle('charity')}>Charity</ToggleButton>
-          }
-          <Divider />
-          {toggle === 'other' ?
-            <ToggleButton active value="other" onClick={() => setToggle('other')}>Other</ToggleButton>
-            :
-            <ToggleButton value="other" onClick={() => setToggle('other')}>Other</ToggleButton>
-          }
+        <ToggleButtonGroup>
+          {['all', 'featured', 'hospitality', 'charity', 'other'].map((value) => (
+            <React.Fragment key={value}>
+              <ToggleButton
+                active={toggle === value}
+                value={value}
+                onClick={() => setToggle(value)}
+              >
+                {value.charAt(0).toUpperCase() + value.slice(1)}
+              </ToggleButton>
+              {value !== 'other' && <Divider />}
+            </React.Fragment>
+          ))}
         </ToggleButtonGroup>
         <CardContainer>
-          {toggle === 'all' && projects
-            .map((project) => (
-              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
-            ))}
           {projects
-            .filter((item) => item.category == toggle)
+            .filter((project) => toggle === 'all' || project.category.includes(toggle))
             .map((project) => (
-              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+              <ProjectCard key={project.id} project={project} openModal={openModal} setOpenModal={setOpenModal} />
             ))}
         </CardContainer>
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
