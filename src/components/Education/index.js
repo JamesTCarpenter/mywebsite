8f78efd07fbfb5cdfcefbe2b014baf82ff4521aa
James Carpenter <jamestimothycarpenter@gmail.com>
2024-11-06 00:43:55 +0000

    Update index.js

--- src/components/Education/index.js
+++ src/components/Education/index.js
@@ -93,8 +93,8 @@ const index = () => {
                                     <EducationCard education={education}/>
                                 </TimelineContent>
                                 <TimelineSeparator>
-                                    <TimelineDot variant="outlined" style={{ border: ' 2px solid #80bf77' }} />
-                                    {index !== experiences.length  && <TimelineConnector style={{ background: '#b4cfb0' }} />}
+                                    <TimelineDot variant="outlined" style={{ border: ' 2px solid #64E79E' }} />
+                                    {index !== experiences.length  && <TimelineConnector style={{ background: '#64E79E' }} />}
                                 </TimelineSeparator>
                             </TimelineItem>
                         ))}
